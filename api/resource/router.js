5f36dc1806c15e3d8cf490df6c27b491c8165821
Sallie McKee <salliemckee@Morrigan.local>
2022-04-22 13:13:07 -0500

    finished getAll resources model

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,17 @@
 // build your `/api/resources` router here
+const express = require("express");
+const router = express.Router();
+const Resource = require("./model");
+
+// router.post((req, res, next) => {});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const resources = await Resource.getAll();
+    res.json(resources);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ const server = express();
 server.use(express.json());
 
 server.use("/api/projects", projectRouter);
-// server.use("/api/resource", resourceRouter);
+server.use("/api/resources", resourceRouter);
 // server.use("/api/task", taskRouter);
 
 server.use("*", (req, res) => {
